f801789e21051fe4b77e312d48a9aa8a945daf2d
gfrobles <gfrobles@gmail.com>
2024-12-15 13:21:09 -0800

    add character book count

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     words = get_book_words(text)
     word_count = len(words)
     print(word_count)
+    character_count = get_book_character(text)
+    print(character_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +14,17 @@ def get_book_text(path):
 def get_book_words(text):
     words = text.split()
     return words
+
+def get_book_character(text):
+    lowered_text = text.lower()
+    characters = {}
+
+    for i in text:
+        if i in characters:
+            characters[i] = characters[i] + 1
+        else:
+            characters[i] = 1
+
+    return characters
 
 main()
